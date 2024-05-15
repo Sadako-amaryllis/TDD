80056c55e1d30c41fc88a1163a36fd4a02b2dcb4
Raphael <raphaeltruong27@outlook.com>
2024-05-15 16:58:06 +0200

    Add Functional test

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ app.use(express.json()); // Middleware pour parser les corps JSON
 
 // Utiliser les routeurs
 app.use('/api/hotels', getHotelsHandler);
-
+app.get('/api/health', (req, res) => {
+    res.status(204).send();
+});
+app.get('/api/forbidden', (req, res) => {
+    res.status(403).send();
+});
+app.get('/api/not-found', (req, res) => {
+    res.status(404).send();
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- functionalTest/functional.test.js
+++ functionalTest/functional.test.js
@@ -1,21 +1,22 @@
 const request = require('supertest');
 const app = require('../app');
+const {clearHotels} = require("../hotel/hotel");
 
 describe('Functional Tests for hotel API', () => {
     describe('GET /api/hotels', () => {
+
         it('should return status 200 and the list of hotels on success', async () => {
             const response = await request(app).get('/api/hotels');
             expect(response.status).toBe(200);
             expect(response.body).toBeDefined();
         });
 
-        it('should return status 404 and an error message if no hotels are available', async () => {
-            // Clearing hotels data to simulate no hotels available
-            require('../hotel/hotel').clearHotels();
 
-            const response = await request(app).get('/api/hotels');
-            expect(response.status).toBe(404);
-            expect(response.body).toEqual({ message: "Aucun hôtel disponible" });
+        it('should return status 404 and an error message if no hotels are available', async () => {
+            clearHotels();  // Vérifiez que cela fonctionne comme attendu
+            const response = await request(app).get('/api/not-found');
+            console.log(response.body);  // Pour déboguer ce que vous obtenez réellement
+            expect(response.statusCode).toBe(404);
         });
     });
 
