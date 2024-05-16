4008fa3dcf2444c04f1a546b6942fffcbdb7b3b5
Raphael <raphaeltruong27@outlook.com>
2024-05-16 09:26:04 +0200

    fix app.js

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const hotelRoutes = require('./hotel/hotel');
-const {getHotelsHandler} = require("./hotel/hotel"); // Importez les routes des hôtels
+const { getHotelsHandler } = require("./hotel/hotel");
 
 const app = express();
 
-app.use(express.json()); // Middleware pour parser les corps JSON
+app.use(express.json());
 
-// Utiliser les routeurs
+// Utilisation de getHotelsHandler comme middleware pour la route '/api/hotels'
+// Assurez-vous que getHotelsHandler est correctement défini pour traiter toutes les requêtes à '/api/hotels'
 app.use('/api/hotels', getHotelsHandler);
+
+// Routes statiques pour des états HTTP spécifiques
 app.get('/api/health', (req, res) => {
     res.status(204).send();
 });
@@ -17,9 +20,13 @@ app.get('/api/forbidden', (req, res) => {
 app.get('/api/not-found', (req, res) => {
     res.status(404).send();
 });
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
 
-module.exports = app;  // Exporter pour les tests
+// Seulement démarrer le serveur si ce fichier est exécuté directement par Node.js
+if (require.main === module) {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app; // Exporter pour les tests
